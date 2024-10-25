49031fdb1c54bc0ed956d521ceb6a7dfdc9a07c0
Aapeli <git@aapelivuorinen.com>
2024-10-25 17:34:53 +0300

    Fix GIS.GetUsers
    Switch lite_users to use geom instead of lat, lng

--- app/backend/src/tests/test_gis.py
+++ app/backend/src/tests/test_gis.py
@@ -3,8 +3,9 @@ import json
 import pytest
 from google.protobuf import empty_pb2
 
+from couchers.materialized_views import refresh_materialized_views_rapid
 from tests.test_communities import testing_communities  # noqa
-from tests.test_fixtures import generate_user, gis_session, testconfig  # noqa
+from tests.test_fixtures import db, generate_user, gis_session, testconfig  # noqa
 
 
 @pytest.fixture(autouse=True)
@@ -12,14 +13,15 @@ def _(testconfig):
     pass
 
 
-class TestGetUsers:
-    @staticmethod
-    def test_GetUsers(testing_communities):
-        _, token = generate_user()
+def test_GetUsers(testing_communities):
+    _, token = generate_user()
 
-        with gis_session(token) as gis:
-            http_body = gis.GetUsers(empty_pb2.Empty())
-            assert http_body.content_type == "application/json"
-            data = json.loads(http_body.data)
-            assert data["type"] == "FeatureCollection"
-            assert len(data["features"]) > 1
+    refresh_materialized_views_rapid(None)
+
+    with gis_session(token) as gis:
+        http_body = gis.GetUsers(empty_pb2.Empty())
+        assert http_body.content_type == "application/json"
+        data = json.loads(http_body.data)
+        print(data)
+        assert data["type"] == "FeatureCollection"
+        assert len(data["features"]) > 1
