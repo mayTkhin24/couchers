ab9663035efff0de9323c364c1101da3cd7dc23a
Aapeli <git@aapelivuorinen.com>
2024-10-25 16:54:27 +0300

    Add basic test for gis.GetUsers

--- app/backend/src/tests/test_gis.py
+++ app/backend/src/tests/test_gis.py
@@ -1,2 +1,25 @@
-# Test that gis and all its protobuf wrappers can be imported properly
-import couchers.servicers.gis  # noqa
+import json
+
+import pytest
+from google.protobuf import empty_pb2
+
+from tests.test_communities import testing_communities  # noqa
+from tests.test_fixtures import generate_user, gis_session, testconfig  # noqa
+
+
+@pytest.fixture(autouse=True)
+def _(testconfig):
+    pass
+
+
+class TestGetUsers:
+    @staticmethod
+    def test_GetUsers(testing_communities):
+        _, token = generate_user()
+
+        with gis_session(token) as gis:
+            http_body = gis.GetUsers(empty_pb2.Empty())
+            assert http_body.content_type == "application/json"
+            data = json.loads(http_body.data)
+            assert data["type"] == "FeatureCollection"
+            assert len(data["features"]) > 1
