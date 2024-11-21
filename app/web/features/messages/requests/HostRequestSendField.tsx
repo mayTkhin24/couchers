4317050cb4f4b16dd75c0d58b982a59f50347311
Aapeli <git@aapelivuorinen.com>
2024-11-20 22:14:45 -0500

    Fix mui import

--- app/web/features/messages/requests/HostRequestSendField.tsx
+++ app/web/features/messages/requests/HostRequestSendField.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@material-ui/core";
+import { Typography } from "@mui/material";
 import Button from "components/Button";
 import ConfirmationDialogWrapper from "components/ConfirmationDialogWrapper";
 import StyledLink from "components/StyledLink";
